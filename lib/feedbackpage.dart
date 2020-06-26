9e86962e9fb5f4aad7d6ef8e2a571f81872bca4d
mahmoud-eslami <eslamimahmoud98@gmail.com>
2020-06-26 13:55:20 +0430

    pubspec.yaml Updated

--- lib/feedbackpage.dart
+++ lib/feedbackpage.dart
@@ -1,7 +1,15 @@
 library feedbackpage;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FeedbackPage extends StatefulWidget {
+  @override
+  _FeedbackPageState createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<FeedbackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
